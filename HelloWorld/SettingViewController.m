33ed2125219b75f94f747bb1fe26f0e43849d902
ljlin <lijianlin1995@gmail.com>
2014-04-29 19:08:33 +0800

    可以修改笔刷透明度和颜色

---
+++ HelloWorld/SettingViewController.m
@@ -0,0 +1,71 @@
+//
+//  SettingViewController.m
+//  HelloWorld
+//
+//  Created by ljlin on 14-4-29.
+//  Copyright (c) 2014年 github.com/touchvg. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+
+@interface SettingViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *brushSlider;
+@property (weak, nonatomic) IBOutlet UISlider *opacitySlider;
+
+@end
+
+@implementation SettingViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self){
+        NSLog(@"settingvc initWithCoder");
+    }
+    return  self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.brushSlider.value = self.helper.lineWidth;
+    self.opacitySlider.value = self.helper.lineAlpha;
+    //self.helper.lineColor = [UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)valueChange:(UISlider*)sender
+{
+
+    switch (sender.tag) {
+        case 0:{
+            self.helper.lineWidth = sender.value;
+            break;
+        }
+        case 1:{
+            self.helper.lineAlpha = sender.value;
+        }
+        default:
+            break;
+    }
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- HelloWorld/ViewController.mm
+++ HelloWorld/ViewController.mm
@@ -8,9 +8,13 @@
 
 #import "ViewController.h"
 #import "GiViewHelper.h"
+#import "SettingViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *mainMage;
+@property (weak, nonatomic) IBOutlet UIView *tempDraw;
+@property (weak, nonatomic) GiPaintView* mPaintView;
+@property (strong, nonatomic) NSArray* colors;
 @end
 
 @implementation ViewController
@@ -20,7 +24,7 @@
     [super viewDidLoad];
     
     GiViewHelper *helper = [GiViewHelper sharedInstance];
-    [helper createGraphView:self.view.bounds :self.view];
+    self.mPaintView = [helper createGraphView:self.mainMage.bounds :self.view];
     helper.command = @"splines";
 }
 
@@ -29,5 +33,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)pencilPressed:(UIButton*)sender
+{
+    NSLog(@"pencilPressed ar %d",sender.tag);
+    
+    GiViewHelper *helper = [GiViewHelper sharedInstance:self.mPaintView];
+    helper.lineColor = self.colors[sender.tag];
+    
+}
+- (IBAction)eraserPressed:(id)sender
+{
+    NSLog(@"eraserPressed");
+}
 
+- (NSArray*)colors
+{
+    if(!_colors){
+        _colors = [[NSArray alloc] initWithObjects:[UIColor blackColor],[UIColor grayColor],[UIColor redColor],nil];
+    }
+    return _colors;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqual:@"segueForSetting"]){
+        ((SettingViewController*)segue.destinationViewController).helper = [GiViewHelper sharedInstance:self.mPaintView];
+    }
+}
 @end
