8d752ba239aafcf42cae65fe4ca39f6bc3031d66
ljlin <lijianlin1995@gmail.com>
2014-05-07 20:21:41 +0800

    Revert "add TouchVG.bundle"
    This reverts commit e3f66645107ecffd1d33408e72ac7f9165555efb.

---
+++ HelloWorld/colorPadView.h
@@ -0,0 +1,13 @@
+//
+//  colorPadView.h
+//  HelloWorld
+//
+//  Created by ljlin on 14-5-3.
+//  Copyright (c) 2014年 github.com/touchvg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GiPaintView.h"
+@interface colorPadView : GiPaintView
+
+@end
